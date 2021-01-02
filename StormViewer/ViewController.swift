d7e74cde1c44d3b7c09802b0ea2de8179bff5624
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-01 21:16:03 -0300

    Add tableView

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     var pictures = [String]()
 
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Picture")
+        
         for item in items {
             if item.hasPrefix("nssl") {
                 pictures.append(item)
@@ -26,6 +28,13 @@ class ViewController: UIViewController {
         }
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
-

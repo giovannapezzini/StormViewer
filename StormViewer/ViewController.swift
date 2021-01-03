e2321fc6cde34949861a119d28a93d24f5ce00f7
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-02 22:12:27 -0300

    Configure tableView cell and didSelectRowAt

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -35,6 +38,14 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailVC = DetailViewController()
+        detailVC.selectedImage = pictures[indexPath.row]
+        
+        navigationController?.pushViewController(detailVC, animated: true)
     }
 }

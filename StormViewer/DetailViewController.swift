a16b526377a56dcf5ef60b196168e51f88fd6a0a
giovannapezzini <giovannarpezzini@gmail.com>
2021-01-06 19:54:37 -0300

    Add func to share selected image

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -21,6 +21,8 @@ class DetailViewController: UIViewController {
         title = "\(selectedPictureNumber) of \(totalPictures)"
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         configureImageView()
     }
     
@@ -32,6 +34,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     func configureImageView() {
